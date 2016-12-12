ed8aab4210fc03734c4516fe6a663ef64b77213c
Mohitkh7 <mohitkh7@gmail.com>
2016-12-12 13:46:53 +0530

    Icon and Splash Screeen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild  } from '@angular/core';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { Nav } from 'ionic-angular';
+
 
 import { HomePage } from '../pages/home/home';
+import { ViewDoctors } from '../pages/view-doctors/view-doctors';
+import { ViewDiseases } from '../pages/view-diseases/view-diseases';
+import { Setting } from '../pages/setting/setting';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+	@ViewChild(Nav) nav: Nav;
   rootPage = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private menu: MenuController) {
+  	this.menu=menu;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  //Open HomePage from sidemenu
+  OpenHome(){
+  	this.menu.close();
+  	this.nav.setRoot(HomePage);
+  }
+  //Open ViewDoctors Page from sidemenu
+  OpenViewDoctors(){
+  	this.menu.close();
+  	this.nav.push(ViewDoctors);
+  }
+  //Open ViewDiseases Page from sidemenu
+  OpenViewDiseases(){
+  	this.menu.close();
+  	this.nav.push(ViewDiseases);
+  }
+  //Open ViewDoctors Page from sidemenu
+  OpenSetting(){
+  	this.menu.close();  		
+  	this.nav.push(Setting);
   }
 }
